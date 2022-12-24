db7cae0a3508f4d99e5c6b5d6f4bb8704c7dc954
Ruben Melo <rubenmelo02@gmail.com>
2022-12-24 15:53:19 -0300

    Add ChessPosition class.

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -4,24 +4,8 @@ using chessboard;
 using game;
 
 
-try
-{
-    Board b = new Board(8, 8);
+ChessPosition cp1 = new('a', 1);
+ChessPosition cp2 = new('c', 7);
 
-    Tower t1 = new Tower(b, Color.Black);
-    Position p1 = new Position(0, 0);
-
-    Tower t2 = new Tower(b, Color.Black);
-    Position p2 = new Position(1, 1);
-
-    b.PutPiece(t1, p1);
-    b.PutPiece(t2, p2);
-
-    Screen.printChessboard(b);
-
-    Console.ReadLine();
-}
-catch (BoardException exception)
-{
-    Console.WriteLine(exception.Message);
-}
+Console.WriteLine(cp1 + " = " + cp1.ToPosition());
+Console.WriteLine(cp2 + " = " + cp2.ToPosition());
